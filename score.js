c1e3b19e3acb4778178b9f94173934ebdd6daaac
Akshit <akshitpjt07@gmail.com>
2023-09-28 17:22:20 +0530

    doneall

--- score.js
+++ score.js
@@ -1,1 +1,12 @@
 // Iteration 8: Making scoreboard functional
+var hey = new URLSearchParams(window.location.search);
+console.log(hey);
+var score = hey.get("score");
+
+var scoreBoard = document.getElementById("score-board");
+scoreBoard.innerHTML = score;
+
+var playagainbutton = document.getElementById("play-again-button");
+playagainbutton.onclick = (evt) => {
+    location.href = "./game.html";
+};
